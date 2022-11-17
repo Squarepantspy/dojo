4c7d2b0770ba6b4c4dbab902b9ac4bef66f1c659
Pablo Gonzalez <prgonzalez@fiuna.edu.py>
2022-11-17 00:24:33 -0300

    removiendo carpeta prebootcamp

--- prebootcamp/2 pre-bootcamp web development/Gonzalez_Pablo_JS_Jugueteo.js
+++
@@ -1,9 +0,0 @@
-var alturaNiño = 52;
-function muestraSiElNiñoPuedeSubirALaMontañaRusa(altura){
-    if ( altura > 52){
-        console.log("Subete chico");
-    }else{
-        console.log("Lo siento chico tal vez el proximo año")
-    }
-}
-muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño);
